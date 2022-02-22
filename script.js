71b3bb65558468cf20eb66e6833d27dd393ab0ef
Masa Orel <orel.masha89@gmail.com>
2022-02-22 20:32:46 +0100

    commit

---
+++ script.js
@@ -0,0 +1,11 @@
+const mobileMenu = () => {
+    const burger = document.querySelector("#burger");
+    const nav = document.querySelector(".nav-links");
+
+    burger.addEventListener("click", ()=> {
+        nav.classList.toggle("active");
+    });
+
+}
+
+mobileMenu();
